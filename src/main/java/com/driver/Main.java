93e1aabbb610c7bd12d080993fe8cd45aefa065b
AntaresScorpius <rohitc171@gamil.com>
2022-10-11 22:12:11 +0530

    Added Overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,17 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         B b = new B();
-        b.meth();
+        System.out.println(b.meth());
     }
-    class A{
-        public String meth(){
-            return "Invoking method from class A";
-        }
+}
+class A{
+    public String meth(){
+        return "Invoking method from class A";
     }
-    class B extends A{
-        public String meth(){
-            return "Method is overridden in Extendend class B";
-        }
+}
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extendend class B";
     }
-  
 }
